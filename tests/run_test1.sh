3d0ce9af4ada11d218b89bdcba03d784462114d0
ahardiag <ahardiag@gmail.com>
2024-09-03 17:42:09 +0200

    Clean tests

--- tests/run_test1.sh
+++ tests/run_test1.sh
@@ -2,27 +2,31 @@
 
 # Test 1 : Simple test to see if the number of permeations is correct
 
-# Output should be :
-# --->    30 events of permeation during 50.000 ns!
-# ---> Relative number of permeations :     6         
+# Define file paths
+LOG_FILE="outputs/test1/permeation_SC8.log"
+CSV_FILE="outputs/test1/permeation_SC8.csv"
 
-# version of python modules used for this test (through a conda envrionement) :
-# - python 3.7.5
-# - MDAnalysis 0.20.1
-# - numpy 1.17.3
-# - matplotlib 3.1.2
-# - pandas 0.25.3
+# Run the permeation script
+../perm_lip.py -r inputs/test1/ref_SC8_6chnd.gro \
+               -f inputs/test1/traj_SC8_6chnd_50ns.xtc \
+               -o test1 \
+               -x "SC8" \
+               -p \
+               -t \
+               -s 2
 
-# Extract informations from water permeation events through the lipid membrane
-../perm_lip.py  -r ref_SC8_6chnd.gro \
-                -f traj_SC8_6chnd_50ns.xtc \
-                -o test1 \
-                -x "SC8" \
-                -p \
-                -t \
-                -s 2
-[[ $? -eq 0 ]] || { exit 1; }
-# Plot the total number of water permeations with respect to time
-python plot_perm_tot.py outputs/test1/permeation_SC8.csv SC8
-
-# For any questions, please contact hardiagon@ibpc.fr
+# Check if the CSV file exists
+if [ -f "$CSV_FILE" ]; then
+    LINE_COUNT=$(wc -l < "$CSV_FILE")
+    
+    # Check if the number of lines is 31 and the log file exists
+    if [ "$LINE_COUNT" -eq 31 ] && [ -f "$LOG_FILE" ]; then
+        echo "Test passed successfully."
+    else
+        echo "Test failed."
+        exit 1
+    fi
+else
+    echo "Test failed."
+    exit 1
+fi
